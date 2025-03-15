ca8f3c44316689290ba03891d6f0c967008cc7a4
kauessilvas <kauesantosds5@gmail.com>
2025-03-15 17:21:36 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿string timeDoCoracao = "Ny Yankees";
+for (int i = 0; i < timeDoCoracao.Length; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.BackgroundColor = ConsoleColor.White;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.Blue;
+    }
+    Console.Write(timeDoCoracao[i]);
+    Console.ResetColor();
+}
